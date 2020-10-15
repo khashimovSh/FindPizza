0ee5a369557f09612d9e93f429f2dd1dcabc8e1e
Shakhzod Khashimov <shoh.hashimov.99@gmail.com>
2020-10-15 13:01:47 +0200

    Updated functions with getting message. Now bot can get Geolocation and send logitude and latitude

--- calculator/src/main/java/com/example/demo/CalcProject.java
+++ calculator/src/main/java/com/example/demo/CalcProject.java
@@ -1,46 +1,62 @@
 package com.example.demo;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendChatAction;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Location;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.Serializable;
 
 public class CalcProject extends TelegramLongPollingBot {
-    private static long chat_id;
+    //public static float longditude;
     @Override
     public void onUpdateReceived(Update update) {
 //        System.out.println(update.getMessage().getText());
 //        System.out.println(update.getMessage().getFrom().getFirstName() +" "+ update.getMessage().getFrom().getLastName());
-
-        chat_id =update.getMessage().getChatId();
-
+        KeyboardButton keyboardButton = new KeyboardButton();
+        keyboardButton.setRequestLocation(true);
         String command = update.getMessage().getText();
+
         SendMessage message = new SendMessage();
-        if(command.equals("/mylastname")){
-            System.out.println(update.getMessage().getFrom().getLastName());
-            message.setText(update.getMessage().getFrom().getLastName());
-        }
-        else if(command.equals("/myfullname")){
-            System.out.println(update.getMessage().getFrom().getFirstName()+" "+update.getMessage().getFrom().getLastName());
-            message.setText(update.getMessage().getFrom().getFirstName()+" "+update.getMessage().getFrom().getLastName());
-        }
-
+//        longditude = update.getMessage().getLocation().getLongitude();
         message.setChatId(update.getMessage().getChatId());
-
+        // updated for making sure if user doesn't enter alphabetic
+        if(command!=null){
         try{
-            message.setText("You entered: " + command);
-            execute(message);
+            String text = command.strip().toLowerCase();
+            if(command.equals("want to eat") ||
+                    command.equals("wonna eat")||
+                    command.equals("hungry")||
+                    command.equals("eat")){
+                message.setText("Please send location for finding restaurants");
+                execute(message);
+            }
 
-            message.setText("The result is: " + (getMsg(command)));
-            execute(message);
         }catch (TelegramApiException e){
             e.printStackTrace();
         }
 
+    }
+        if(update.getMessage().getLocation() != null){
+            try{
+                float longitude = update.getMessage().getLocation().getLongitude();
+                float latitude = update.getMessage().getLocation().getLatitude();
+                message.setText("McDonalds 200m\n" +
+                        "Burger King 100m\n" +
+                        "KFC 500m\n"+longitude+" "+latitude);
+                execute(message);
+            }catch (TelegramApiException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     private float getMsg(String msg){
@@ -61,4 +77,6 @@ public class CalcProject extends TelegramLongPollingBot {
     public String getBotToken() {
         return "1218119673:AAHf2ZCkLy2RpX_qZgzW-x8tmy4pM0zwMjU";
     }
+
+
 }
